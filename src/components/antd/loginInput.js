0f58fac3f799c4dbaa309fdb9b2d597746d5b41d
Yash-Raj-50 <yashraj44777@gmail.com>
2024-01-02 21:59:20 +0530

    Guest Mode Added

--- src/components/antd/loginInput.js
+++ src/components/antd/loginInput.js
@@ -1,7 +1,7 @@
 'use client';
 
 import React from 'react';
-import { Button, Checkbox, Form, Input } from 'antd';
+import { Button, Form, Input } from 'antd';
 import '../../app/globals.css';
 // import { signIn } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
@@ -29,6 +29,22 @@ const LoginInput = () => {
   const onFinishFailed = (errorInfo) => {
     console.log('Failed:', errorInfo);
   };
+
+  const GuestInfo = {email: 'guest@quicknews.demo', password: 'test123'};
+  const GuestLogin = async () => {
+    try {
+      const res = await signInWithEmailAndPassword(GuestInfo.email, GuestInfo.password);
+      if(typeof window !== 'undefined') {
+        sessionStorage.setItem('user_email', res.user.email);
+        sessionStorage.setItem('user_id', res.user.uid);
+        sessionStorage.setItem('isGuest', true);
+      }
+      router.push('/');
+    } catch(e) {
+      console.error(e);
+    }
+  };
+  
   return (
     <Form
       name="basic"
@@ -84,8 +100,18 @@ const LoginInput = () => {
           span: 16,
         }}
       >
-        <Button type="default" htmlType="submit" className='bg-indigo-300 text-white text-xl h-auto hover:scale-110'>
+        <Button type="default" htmlType="submit" className='bg-indigo-400 text-white text-xl h-auto hover:scale-110'>
           Log In
+        </Button>
+      </Form.Item>
+      <Form.Item
+        wrapperCol={{
+          offset: 2,
+          span: 16,
+        }}
+      >
+        <Button type="default" onClick={GuestLogin} className='bg-emerald-500 text-white text-xl h-auto hover:scale-110'>
+          Log In as Guest (Universal)
         </Button>
       </Form.Item>
     </Form>
